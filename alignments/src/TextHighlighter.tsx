88e07facb8de83f1315f4206486ed9d34b1e8fe1
ryderwishart <ryderwishart@gmail.com>
2023-09-28 14:33:00 -0700

    Pass in multiple verses and filter sidebar query by tokens

--- alignments/src/TextHighlighter.tsx
+++ alignments/src/TextHighlighter.tsx
@@ -1,7 +1,7 @@
 import './TextHighlighter.css';
-
 import React, { useState, useEffect } from 'react';
 import Highlighter from 'react-highlight-words';
+import Sidebar from './Sidebar';
 
 /* example VerseData instance :
 
@@ -162,7 +162,7 @@ interface Range {
   endPosition: number;
 }
 
-interface VerseData {
+export interface VerseData {
   vref: string;
   bsb: Verse;
   macula: Verse;
@@ -173,7 +173,8 @@ interface VerseData {
 
 interface Alignment {
   'English phrase': Phrase;
-  'Hebrew phrase': Phrase;
+  'Hebrew phrase'?: Phrase;
+  'Greek phrase'?: Phrase;
   'Target phrase': Phrase;
   'Pseudo-English phrase'?: string;
 }
@@ -186,6 +187,8 @@ interface Phrase {
 const TextHighlighter: React.FC = () => {
   const [data, setData] = useState<VerseData[]>([]);
   const [activePhrase, setActivePhrase] = useState<Alignment | null>();
+  const [activeVerse, setActiveVerse] = useState<VerseData | null>();
+  const [selectedTokenIds, setSelectedTokenIds] = useState<string[]>([]);
 
   useEffect(() => {
     fetch('/data.jsonl')
@@ -201,60 +204,128 @@ const TextHighlighter: React.FC = () => {
     setActivePhrase(alignment);
   };
 
+  const handlePhraseClick = (currentVerse: VerseData, alignment: Alignment) => {
+    setActiveVerse(currentVerse);
+
+    // Extract the phrases from the alignment
+    const phrases = ['Greek phrase', 'Hebrew phrase']
+      .map((key: string) => alignment[key])
+      .filter(Boolean);
+
+    // Initialize an empty array to store the matching ids
+    const matchingIds: string[] = [];
+
+    // Iterate over each phrase
+    phrases.forEach((phrase) => {
+      // Iterate over each range in the phrase
+      phrase.ranges.forEach(
+        (range: { endPosition: number; startPosition: number }) => {
+          // Iterate over each token in the current verse
+          currentVerse.macula.token_ids.forEach((token) => {
+            // Check if the token's range overlaps with the phrase's range
+            if (
+              token.range.startPosition <= range.endPosition &&
+              token.range.endPosition >= range.startPosition
+            ) {
+              // If it does, add the token's id to the matching ids
+              matchingIds.push(token.id);
+            }
+          });
+        },
+      );
+    });
+
+    // Set the selected token ids to the matching ids
+    setSelectedTokenIds(matchingIds);
+  };
+
   return (
-    <div>
-      {data.map((item, index) => (
-        <div key={index} className="text-block">
-          <div className="english">
-            {item.alignment.map((alignment: Alignment, aIdx: number) => (
-              <span
-                key={aIdx}
-                onMouseEnter={() => handlePhraseHover(alignment)}
-                onMouseLeave={() => handlePhraseHover(null)}
+    <>
+      {/* <pre>{JSON.stringify(data[0], null, 2)}</pre> */}
+      <div className="p-4 bg-gray-100">
+        {data.map(
+          (item, index) =>
+            index < 10 && (
+              <div
+                key={index}
+                className="text-block p-4 bg-white shadow-lg rounded-lg my-4"
               >
-                {alignment['English phrase']['original-text-value']}
-              </span>
-            ))}
-          </div>
-          <div className="bsb">
-            <Highlighter
-              highlightClassName="highlight"
-              searchWords={[
-                activePhrase?.['English phrase']?.['original-text-value'] || '',
-                activePhrase?.['Hebrew phrase']?.['original-text-value'] || '',
-                activePhrase?.['Target phrase']?.['original-text-value'] || '',
-              ]}
-              autoEscape={true}
-              textToHighlight={item.bsb.content}
-            />
-          </div>
-          <div className="macula">
-            <Highlighter
-              highlightClassName="highlight"
-              searchWords={[
-                activePhrase?.['English phrase']?.['original-text-value'] || '',
-                activePhrase?.['Hebrew phrase']?.['original-text-value'] || '',
-                activePhrase?.['Target phrase']?.['original-text-value'] || '',
-              ]}
-              autoEscape={true}
-              textToHighlight={item.macula.content}
-            />
-          </div>
-          <div className="target">
-            <Highlighter
-              highlightClassName="highlight"
-              searchWords={[
-                activePhrase?.['English phrase']?.['original-text-value'] || '',
-                activePhrase?.['Hebrew phrase']?.['original-text-value'] || '',
-                activePhrase?.['Target phrase']?.['original-text-value'] || '',
-              ]}
-              autoEscape={true}
-              textToHighlight={item.target.content}
-            />
-          </div>
-        </div>
-      ))}
-    </div>
+                <div className="macula">
+                  {item.alignment.map((alignment: Alignment, aIdx: number) => (
+                    <span
+                      key={aIdx}
+                      onMouseEnter={() => handlePhraseHover(alignment)}
+                      onMouseLeave={() => handlePhraseHover(null)}
+                      onClick={() => handlePhraseClick(item, alignment)}
+                      className="cursor-pointer text-blue-600 hover:text-blue-800"
+                    >
+                      {aIdx}
+                    </span>
+                  ))}
+                </div>
+                <div className="bsb">
+                  <Highlighter
+                    highlightClassName="highlight bg-yellow-300"
+                    searchWords={[
+                      activePhrase?.['English phrase']?.[
+                        'original-text-value'
+                      ] || '',
+                      activePhrase?.['Hebrew phrase']?.[
+                        'original-text-value'
+                      ] || '',
+                      activePhrase?.['Target phrase']?.[
+                        'original-text-value'
+                      ] || '',
+                    ]}
+                    autoEscape={true}
+                    textToHighlight={item.bsb.content}
+                  />
+                </div>
+                <div className="macula">
+                  <Highlighter
+                    highlightClassName="highlight bg-yellow-300"
+                    searchWords={[
+                      activePhrase?.['English phrase']?.[
+                        'original-text-value'
+                      ] || '',
+                      activePhrase?.['Hebrew phrase']?.[
+                        'original-text-value'
+                      ] || '',
+                      activePhrase?.['Target phrase']?.[
+                        'original-text-value'
+                      ] || '',
+                    ]}
+                    autoEscape={true}
+                    textToHighlight={item.macula.content}
+                  />
+                </div>
+                <div className="target">
+                  <Highlighter
+                    highlightClassName="highlight bg-yellow-300"
+                    searchWords={[
+                      activePhrase?.['English phrase']?.[
+                        'original-text-value'
+                      ] || '',
+                      activePhrase?.['Hebrew phrase']?.[
+                        'original-text-value'
+                      ] || '',
+                      activePhrase?.['Target phrase']?.[
+                        'original-text-value'
+                      ] || '',
+                    ]}
+                    autoEscape={true}
+                    textToHighlight={item.target.content}
+                  />
+                </div>
+              </div>
+            ),
+        )}
+      </div>
+      <div className="p-4 bg-white shadow-lg rounded-lg my-4">
+        {JSON.stringify(selectedTokenIds)}
+      </div>
+      <Sidebar verseData={activeVerse} tokenIds={selectedTokenIds} />
+    </>
   );
 };
 
