a54da3f0037fc8011dac113ac6a899289a0bbe83
ryderwishart <ryderwishart@gmail.com>
2023-09-29 09:34:37 -0700

    Use React 18 main render

--- alignments/src/main.tsx
+++ alignments/src/main.tsx
@@ -1,14 +1,20 @@
 // src/main.tsx
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import TextHighlighter from './TextHighlighter';
 import './TextHighlighter.css';
 import './index.css';
 
-const App: React.FC = () => (
+// const App: React.FC = () => (
+//   <div className="App p-4">
+//     <TextHighlighter />
+//   </div>
+// );
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(
   <div className="App p-4">
     <TextHighlighter />
-  </div>
+  </div>,
 );
-
-ReactDOM.render(<App />, document.getElementById('root'));
