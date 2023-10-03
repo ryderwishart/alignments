c69b0b21c2f98e8d5c17038cd683ca67081efac7
ryderwishart <ryderwishart@gmail.com>
2023-10-03 12:18:18 -0700

    Add alt-display checkbox

--- alignments/src/TextHighlighter.tsx
+++ alignments/src/TextHighlighter.tsx
@@ -79,6 +79,7 @@ const TextHighlighter: React.FC = () => {
   const [activePhrase, setActivePhrase] = useState<Alignment | null>();
   const [activeVerse, setActiveVerse] = useState<VerseData | null>();
   const [selectedTokenIds, setSelectedTokenIds] = useState<string[]>([]);
+  const [showAlt, setShowAlt] = useState(false);
 
   // const data_path = '/ranges-for-alignments chunk 2.jsonl';
   // useEffect(() => {
@@ -103,75 +104,100 @@ const TextHighlighter: React.FC = () => {
     setActiveVerse(currentVerse);
 
     // Extract the phrases from the alignment
-    const phrase = alignment['Macula phrase']
+    const phrase = alignment['Macula phrase'];
     // Initialize an empty array to store the matching ids
     const matchingIds: string[] = [];
 
     // Iterate over each phrase
     // phrases.forEach((phrase) => {
-      // Iterate over each range in the phrase
-      phrase?.ranges.forEach(
-        (range: { endPosition: number; startPosition: number }) => {
-          // Iterate over each token in the current verse
-          currentVerse?.macula?.token_ids?.forEach((token) => {
-            // Check if the token's range overlaps with the phrase's range
-            if (
-              token.range.startPosition <= range.endPosition &&
-              token.range.endPosition >= range.startPosition
-            ) {
-              // If it does, add the token's id to the matching ids
-              matchingIds.push(token.id);
-            }
-          });
-        },
-      );
+    // Iterate over each range in the phrase
+    phrase?.ranges.forEach(
+      (range: { endPosition: number; startPosition: number }) => {
+        // Iterate over each token in the current verse
+        currentVerse?.macula?.token_ids?.forEach((token) => {
+          // Check if the token's range overlaps with the phrase's range
+          if (
+            token.range.startPosition <= range.endPosition &&
+            token.range.endPosition >= range.startPosition
+          ) {
+            // If it does, add the token's id to the matching ids
+            matchingIds.push(token.id);
+          }
+        });
+      },
+    );
     // });
 
     // Set the selected token ids to the matching ids
     setSelectedTokenIds(matchingIds);
   };
 
-  const fetchVerseIndicesBySearchString = async (searchString: string, limit = 10) => {
+  const fetchVerseIndicesBySearchString = async (
+    searchString: string,
+    limit = 10,
+  ) => {
     const indicesFound: number[] = [];
-    const responses = await Promise.all([fetch('/data-chunk-NT.jsonl'), fetch('/data-chunk-OT-1.jsonl'), fetch('/data-chunk-OT-2.jsonl')]);
-    const texts = await Promise.all(responses.map(response => response.text()));
-    const lines = texts.flatMap(text => text.split('\n').filter((line) => line));
+    const responses = await Promise.all([
+      fetch('/data-chunk-NT.jsonl'),
+      fetch('/data-chunk-OT-1.jsonl'),
+      fetch('/data-chunk-OT-2.jsonl'),
+    ]);
+    const texts = await Promise.all(
+      responses.map((response) => response.text()),
+    );
+    const lines = texts.flatMap((text) =>
+      text.split('\n').filter((line) => line),
+    );
 
     lines.forEach((line, index) => {
-      if(line.toLowerCase().includes(searchString.toLowerCase()) && indicesFound.length < limit) {
+      if (
+        line.toLowerCase().includes(searchString.toLowerCase()) &&
+        indicesFound.length < limit
+      ) {
         indicesFound.push(index);
       }
     });
     return indicesFound;
-  }
+  };
 
   const fetchVersesByIndices = async (indices: number[]) => {
     const jsonData: VerseData[] = [];
-    const responses = await Promise.all([fetch('/data-chunk-NT.jsonl'), fetch('/data-chunk-OT-1.jsonl'), fetch('/data-chunk-OT-2.jsonl')]);
-    const texts = await Promise.all(responses.map(response => response.text()));
-    const lines = texts.flatMap(text => text.split('\n').filter((line) => line));
+    const responses = await Promise.all([
+      fetch('/data-chunk-NT.jsonl'),
+      fetch('/data-chunk-OT-1.jsonl'),
+      fetch('/data-chunk-OT-2.jsonl'),
+    ]);
+    const texts = await Promise.all(
+      responses.map((response) => response.text()),
+    );
+    const lines = texts.flatMap((text) =>
+      text.split('\n').filter((line) => line),
+    );
 
     indices.forEach((index) => {
       try {
         const parsedLine = JSON.parse(lines[index]);
         jsonData.push(parsedLine);
       } catch (error) {
-        console.error(`Error parsing line at index ${index}: ${lines[index]}`, error);
+        console.error(
+          `Error parsing line at index ${index}: ${lines[index]}`,
+          error,
+        );
       }
     });
 
     return jsonData;
-  }
-// todo: bold the string that was searched
-  const fetchVerseAndSiblingsByVref = async (searchString:string) => {
-    console.log({searchString})
+  };
+  // todo: bold the string that was searched
+  const fetchVerseAndSiblingsByVref = async (searchString: string) => {
+    console.log({ searchString });
     // fetch verse by veref
-    // fetch verse before verse in question and verse afer vers in question 
-    const verseIndices = await fetchVerseIndicesBySearchString(searchString)
-    console.log({verseIndex: verseIndices})
-    const versesToDisplay = await fetchVersesByIndices(verseIndices)
-    setVersesToDisplay(versesToDisplay)
-  }
+    // fetch verse before verse in question and verse afer vers in question
+    const verseIndices = await fetchVerseIndicesBySearchString(searchString);
+    console.log({ verseIndex: verseIndices });
+    const versesToDisplay = await fetchVersesByIndices(verseIndices);
+    setVersesToDisplay(versesToDisplay);
+  };
 
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setInputValue(event.target.value);
@@ -185,122 +211,137 @@ const TextHighlighter: React.FC = () => {
   return (
     <div className="flex flex-row">
       <div className="p-4 bg-gray-100 w-3/4">
-        <h2 className="text-2xl mb-4">Aligned verses (total: {versesToDisplay.length})</h2>
+        <h2 className="text-2xl mb-4">
+          Aligned verses (total: {versesToDisplay.length})
+        </h2>
         {/* Add a form with an input and a submit button */}
-        <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 flex flex-row gap-2" onSubmit={handleInputSubmit}>
+        <form
+          className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 flex flex-row gap-2"
+          onSubmit={handleInputSubmit}
+        >
           <input
             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
             type="text"
             value={inputValue}
             onChange={handleInputChange}
           />
-          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">Submit</button>
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            type="submit"
+          >
+            Submit
+          </button>
         </form>
-        {versesToDisplay.map(
-          (item, index) => (
-              <div
-                key={index}
-                className="text-block p-4 bg-white shadow-lg rounded-lg my-4"
-                onMouseEnter={() => handleVerseHover(item)}
-                onMouseLeave={() => handleVerseHover(null)}
-              >
-                <span>{item.vref}</span>
-                <div className="macula">
-                  {item.alignment.map((alignment: Alignment, aIdx: number) => (
+        {/* Add a checkbox to toggle the showAlt state */}
+        <div className="flex items-center mb-4">
+          <input
+            className="mr-2 leading-tight"
+            type="checkbox"
+            checked={showAlt}
+            onChange={() => setShowAlt(!showAlt)}
+          />
+          <span className="text-sm">Show pseudo-English</span>
+        </div>
+        {versesToDisplay.map((item, index) => (
+          <div
+            key={index}
+            className="text-block p-4 bg-white shadow-lg rounded-lg my-4"
+            onMouseEnter={() => handleVerseHover(item)}
+            onMouseLeave={() => handleVerseHover(null)}
+          >
+            <span>{item.vref}</span>
+            <div className="macula">
+              {item.alignment.map((alignment: Alignment, aIdx: number) => (
+                <span
+                  key={aIdx}
+                  onMouseEnter={() => handlePhraseHover(alignment)}
+                  onMouseLeave={() => handlePhraseHover(null)}
+                  onClick={() => handlePhraseClick(item, alignment)}
+                  className="cursor-pointer text-blue-600 hover:text-blue-800"
+                >
+                  Translation unit {aIdx + 1}
+                  {aIdx + 1 < item.alignment.length && ' | '}
+                </span>
+              ))}
+            </div>
+            {showAlt && (
+              <div className="italic text-orange-700">{item.alt}</div>
+            )}
+            <div className="bsb-ner flex flex-row gap-2">
+              {
+                // add a tiny red badge if ner is present and len > 0,
+                item.bsb.ner && item.bsb.ner.length > 0 && (
+                  <span className="text-gray-100 px-1 rounded-l bg-red-500">
+                    concepts:
+                  </span>
+                )
+              }
+              {item.vref === activeVerse?.vref &&
+                item.bsb.ner?.map((ner: NamedEntity, nerIdx: number) => {
+                  //   const color: string = colors_for_labels[ner.label];
+                  return (
                     <span
-                      key={aIdx}
-                      onMouseEnter={() => handlePhraseHover(alignment)}
-                      onMouseLeave={() => handlePhraseHover(null)}
-                      onClick={() => handlePhraseClick(item, alignment)}
-                      className="cursor-pointer text-blue-600 hover:text-blue-800"
+                      key={nerIdx}
+                      className={`text-gray-100 px-1 rounded bg-purple-300`}
                     >
-                      Translation unit {aIdx + 1}
-                      {aIdx + 1 < item.alignment.length && ' | '}
+                      {ner.name} {ner.label}
                     </span>
-                  ))}
-                </div>
-                <div className="bsb-ner flex flex-row gap-2">
-                  {
-                    // add a tiny red badge if ner is present and len > 0,
-                    item.bsb.ner && item.bsb.ner.length > 0 && (
-                      <span className="text-gray-100 px-1 rounded-l bg-red-500">
-                        concepts:
-                      </span>
-                    )
-                  }
-                  {item.vref === activeVerse?.vref &&
-                    item.bsb.ner?.map((ner: NamedEntity, nerIdx: number) => {
-                      //   const color: string = colors_for_labels[ner.label];
-                      return (
-                        <span
-                          key={nerIdx}
-                          className={`text-gray-100 px-1 rounded bg-purple-300`}
-                        >
-                          {ner.name} {ner.label}
-                        </span>
-                      );
-                    })}
-                </div>
-                <div className="bsb">
-                  <Highlighter
-                    highlightClassName="highlight bg-yellow-300"
-                    searchWords={[
-                      activePhrase?.['English phrase']?.[
-                        'original-text-value'
-                      ] || '',
-                      activePhrase?.['Macula phrase']?.[
-                        'original-text-value'
-                      ] || '',
-                      activePhrase?.['Target phrase']?.[
-                        'original-text-value'
-                      ] || '',
-                    ]}
-                    autoEscape={true}
-                    textToHighlight={item.bsb.content}
-                  />
-                </div>
-                <div className="macula">
-                  <Highlighter
-                    highlightClassName="highlight bg-yellow-300"
-                    searchWords={[
-                      activePhrase?.['English phrase']?.[
-                        'original-text-value'
-                      ] || '',
-                      activePhrase?.['Macula phrase']?.[
-                        'original-text-value'
-                      ] || '',
-                      activePhrase?.['Target phrase']?.[
-                        'original-text-value'
-                      ] || '',
-                    ]}
-                    autoEscape={true}
-                    textToHighlight={item.macula.content}
-                  />
-                </div>
-                <div className="target">
-                  <Highlighter
-                    highlightClassName="highlight bg-yellow-300"
-                    searchWords={[
-                      activePhrase?.['English phrase']?.[
-                        'original-text-value'
-                      ] || '',
-                      activePhrase?.['Macula phrase']?.[
-                        'original-text-value'
-                      ] || '',
-                      activePhrase?.['Target phrase']?.[
-                        'original-text-value'
-                      ] || '',
-                    ]}
-                    autoEscape={true}
-                    textToHighlight={item.target.content}
-                  />
-                </div>
-              </div>
-            ),
-        )}
+                  );
+                })}
+            </div>
+            <div className="bsb">
+              <Highlighter
+                highlightClassName="highlight bg-yellow-300"
+                searchWords={[
+                  activePhrase?.['English phrase']?.['original-text-value'] ||
+                    '',
+                  activePhrase?.['Macula phrase']?.['original-text-value'] ||
+                    '',
+                  activePhrase?.['Target phrase']?.['original-text-value'] ||
+                    '',
+                ]}
+                autoEscape={true}
+                textToHighlight={item.bsb.content}
+              />
+            </div>
+            <div className="macula">
+              <Highlighter
+                highlightClassName="highlight bg-yellow-300"
+                searchWords={[
+                  activePhrase?.['English phrase']?.['original-text-value'] ||
+                    '',
+                  activePhrase?.['Macula phrase']?.['original-text-value'] ||
+                    '',
+                  activePhrase?.['Target phrase']?.['original-text-value'] ||
+                    '',
+                ]}
+                autoEscape={true}
+                textToHighlight={item.macula.content}
+              />
+            </div>
+            <div className="target">
+              <Highlighter
+                highlightClassName="highlight bg-yellow-300"
+                searchWords={[
+                  activePhrase?.['English phrase']?.['original-text-value'] ||
+                    '',
+                  activePhrase?.['Macula phrase']?.['original-text-value'] ||
+                    '',
+                  activePhrase?.['Target phrase']?.['original-text-value'] ||
+                    '',
+                ]}
+                autoEscape={true}
+                textToHighlight={item.target.content}
+              />
+            </div>
+          </div>
+        ))}
       </div>
       <div className="px-4 bg-white fixed right-0 w-1/4">
-        <Sidebar verseData={activeVerse || undefined} tokenIds={selectedTokenIds} />
+        <Sidebar
+          verseData={activeVerse || undefined}
+          tokenIds={selectedTokenIds}
+        />
       </div>
     </div>
   );
